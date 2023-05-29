e040ce49d769d3dd571619b0a9ab62008e7073ff
Tony Benoy <me@tonybenoy.com>
2023-05-29 19:46:03 +0300

    Sample sentiment analysis(untested)

--- tweet.py
+++ tweet.py
@@ -2,11 +2,10 @@ from snscrape.modules.twitter import TwitterUserScraper
 import pandas as pd
 user = 'tonybenoy'
 list_tweets = []
-count = 1000 # Use a date logic in loop instead of count
+count = 10 # Use a date logic in loop instead of count
 tweets = TwitterUserScraper(user).get_items()
 for tweet in tweets:
-    print(tweet.json())
-    list_tweets.append(tweet.json()) # Refer here for all possible values https://thetechrobo.ca/snscrape-docs/_autosummary/snscrape.modules.twitter.Tweet.html#snscrape.modules.twitter.Tweet
+    list_tweets.append({"content":tweet.rawContent,"replyCount":tweet.replyCount}) # Refer here for all possible values https://thetechrobo.ca/snscrape-docs/_autosummary/snscrape.modules.twitter.Tweet.html#snscrape.modules.twitter.Tweet
     count -= 1
     if count == 0: # Use a date logic in loop instead of count
         break
