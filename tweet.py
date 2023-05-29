405e6261c8a6ffb24043b3c244ace6e735ba3a23
Tony Benoy <me@tonybenoy.com>
2023-05-29 19:17:17 +0300

    Working tweet code

---
+++ tweet.py
@@ -0,0 +1,13 @@
+from snscrape.modules.twitter import TwitterUserScraper
+import pandas as pd
+user = 'tonybenoy'
+list_tweets = []
+count = 1000 # Use a date logic in loop instead of count
+tweets = TwitterUserScraper(user).get_items()
+for tweet in tweets:
+    print(tweet.json())
+    list_tweets.append(tweet.json()) # Refer here for all possible values https://thetechrobo.ca/snscrape-docs/_autosummary/snscrape.modules.twitter.Tweet.html#snscrape.modules.twitter.Tweet
+    count -= 1
+    if count == 0: # Use a date logic in loop instead of count
+        break
+df = pd.DataFrame(list_tweets)
